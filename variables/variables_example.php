5f6095c3156ca775c6a5858c2e60963e9f0d6917
Mauro Vargas <maurovargas7725@gmail.com>
2021-05-12 17:56:28 -0600

    Cover variables in a php file

---
+++ variables/variables_example.php
@@ -0,0 +1,15 @@
+
+<?php
+/*
+Variables-A way to store information in order to reference it later
+*/
+  $age = 20;
+  echo "<br>";
+  echo $age; // Outputs 20
+  $age = 40;
+  echo "<br>";
+  echo $age; // Outputs 40
+  echo "<br>";
+  $double_age = $age * 2; //Outputs 80
+  echo $double_age;
+?>
